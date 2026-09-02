8201242fa31d343f61c5389f0cd6939f2d2eae6a
8113jsl-ui <8113jsl@gmail.com>
2026-09-02 17:33:39 +0900

    Update dict_practice.py

--- dict_practice.py
+++ dict_practice.py
@@ -23,8 +23,9 @@ for pet in pets:
 
 
 # 문제 3. numbers 내부에 들어있는 숫자가 몇 번 등장하는지 세기
+
 # (방법 1) count()
-# (방법 2) +=
+
 
 
 # 숫자는 무작위로 입력해도 상관 없습니다.
@@ -43,6 +44,27 @@ for i in numbers:
 #    count += i
 #    counter[i] = count
 
+print(counter)
+
+
+
+# 문제 3. numbers 내부에 들어있는 숫자가 몇 번 등장하는지 세기
+
+# (방법 2) +=
+
+# 숫자는 무작위로 입력해도 상관 없습니다.
+numbers = [1,2,6,8,4,3,2,1,9,5,4,9,7,2,1,3,5,4,8,9,7,2,3]
+counter = {}
+
+for number in numbers:
+#TODO: 여기에 코드를 작성하세요.
+# 최종 출력
+     if number not in counter:
+          counter[number] = 1                    # counter에 없던 숫자는 1을 할당하세요! -> 1로 시작하세요!
+     else:
+          counter[number] += 1                   # counter에 이미 있는 숫자 -> 나올 때마다 1씩 더하세요!
+     counter[number] = counter[number]
+     
 print(counter)
 
 
